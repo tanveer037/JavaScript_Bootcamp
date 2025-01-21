b4d7dfa61f82235add821675b8dcc11a5f07597b
tanveer037 <tanveerdedhaka@gmail.com>
2025-01-21 13:16:52 +0600

    feat #1 : learned datatypes

---
+++ issue_1/datatypes.js
@@ -0,0 +1,25 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person)
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+//When adding a number and a string, JavaScript will treat the number as a string.
+let a = 16 + ' ' + "Volvo";
+console.log(a)
