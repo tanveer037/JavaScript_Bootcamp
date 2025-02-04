990b34718957bce71e71bf3604171fad5658e358
tanveer037 <tanveerdedhaka@gmail.com>
2025-02-04 22:17:34 +0600

    feat #3 : updated sandbox.js

--- issue_3/sandbox.js
+++ issue_3/sandbox.js
@@ -4,18 +4,13 @@ let text = null
 
 request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState)
-    if (request.readyState === 4){
-        text = request.responseText
-    }
-    else {
-        text = 'null'
+    if (request.readyState === 4 && request.status === 200){
+        console.log(request.responseText)
+    }else if(request.readyState === 4){
+        console.log('Could not fetch data')
     }
 })
 
 
 request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
 request.send()
-
-function myFunction(){
-    document.getElementById("demo").innerHTML = text
-}
