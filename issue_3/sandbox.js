4d3c192eabeac2ec3218f6380caa46bf2bc9162b
tanveer037 <tanveerdedhaka@gmail.com>
2025-02-04 22:03:59 +0600

    feat #3 : just had some fun pulling data from jsonplaceholder and putting it on page

--- issue_3/sandbox.js
+++ issue_3/sandbox.js
@@ -1,9 +1,21 @@
 //this block of code won't run in node js because XMLHttpRequest is not available in node js package
 const request = new XMLHttpRequest()
+let text = null
 
 request.addEventListener('readystatechange', () => {
-    console.log(request, request.readyState)
+    // console.log(request, request.readyState)
+    if (request.readyState === 4){
+        text = request.responseText
+    }
+    else {
+        text = 'null'
+    }
 })
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos')
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
 request.send()
+
+function myFunction(){
+    document.getElementById("demo").innerHTML = text
+}
