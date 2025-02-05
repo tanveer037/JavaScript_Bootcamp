0e1f523edd95ebaf5837234cf7072cf41310301d
tanveer037 <tanveerdedhaka@gmail.com>
2025-02-05 13:02:00 +0600

    feat #3 : updated sandbox.js and added a callback example file

--- issue_3/sandbox.js
+++ issue_3/sandbox.js
@@ -1,16 +1,20 @@
 //this block of code won't run in node js because XMLHttpRequest is not available in node js package
-const request = new XMLHttpRequest()
-let text = null
+function getTodos(){
+    const request = new XMLHttpRequest()
+    let text = null
+    
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState)
+        if (request.readyState === 4 && request.status === 200){
+            console.log(request.responseText)
+        }else if(request.readyState === 4){
+            console.log('Could not fetch data')
+        }
+    })
+    
+    
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
+    request.send()
+}
 
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState)
-    if (request.readyState === 4 && request.status === 200){
-        console.log(request.responseText)
-    }else if(request.readyState === 4){
-        console.log('Could not fetch data')
-    }
-})
-
-
-request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
-request.send()
+getTodos()
